3d8245e99323780b80a666b201a6b912722701bc
thaiphamitptit <phamthai.itptit@gmail.com>
2024-06-30 22:48:24 +0700

    init

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+  <div class="wrapper">
+    <h1>Vue</h1>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
+
+export default defineConfig({
+  plugins: [vue()],
+  server: {
+    port: 3000
+  },
+  css: {
+    devSourcemap: true
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  }
+})
